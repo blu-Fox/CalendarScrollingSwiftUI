d0754329a73e960f463ca3c87f10466c31c0faef
Jan Stehlik <jan_stehlik@icloud.com>
2022-07-26 19:40:29 +0200

    Initial Commit

---
+++ Shared/DragGestureTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGestureTestApp.swift
+//  Shared
+//
+//  Created by Jan Stehlík on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct DragGestureTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
